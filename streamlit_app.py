87d3bea00dc87f2cf20f1fca9b62793892a037c5
UnnMMO <119270960+UnnMMO@users.noreply.github.com>
2022-12-07 18:54:54 -0500

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from PIL import Image
+
+@st.cache
+def load_model():
+    # El modelo se lee de la carpeta 'model'
+    saved_model = keras.models.load_model('path_to_my_model.h5')
+    return saved_model
+
+
+saved_model = load_model()
+    
+btn = col2.button('Generar imágen aleatoria')
+    if btn:
+      from matplotlib import pyplot as plt
+      plt.imshow(saved_model.gen_image[11].reshape(28,28), interpolation='none')
+      plt.show()
+
+    
+
