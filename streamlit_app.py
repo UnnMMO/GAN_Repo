6528574b8eb68c68c4278570ce045f39f7fbac25
UnnMMO <119270960+UnnMMO@users.noreply.github.com>
2022-12-07 20:23:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ if btn:
     #plt.imshow(gen_image[0].reshape(28,28), interpolation='none')
     plt.imshow(gen_image, interpolation='none')
     plt.show()
-    st.image(gen_image[0].reshape(10,10))
+    st.image(gen_image.reshape(10,10))
     #st.image(plt.imshow(gen_image, interpolation='none'))
 
     
